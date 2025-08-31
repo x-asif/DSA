76dbafc7bae98933bfb0589bd33c78cffd15ab5d
Mohammad Asif <asifmohammad85156@gmail.com>
2025-08-31 22:35:41 +0530

    binary search code is modified

--- BINARY_SEARCH/bs_with_return.c
+++ BINARY_SEARCH/bs_with_return.c
@@ -22,31 +22,34 @@ int binary_search(int*a, int n, int l, int r, int k)
 #include <stdio.h>
 int main ()
 {
-    int* a , l, r, n, m , k;
+    int a[100] , l, r, n, m , k;
     printf("Enter the array size:\n");
     scanf("%d", &n);
     printf("Enter the searching element:\n");
     scanf("%d", &k);
-    printf("Enter the array elements:");
+    printf("Enter the array elements: \n");
     for(int i = 0; i < n ; i++){
         scanf("%d",&a[i]);
     }
-    printf("Entered array is given:");
+    printf("Entered array is given: \n");
     for(int i = 0; i<n ; i++){
-        printf("%d",a[i]);
+        printf("%d \t",a[i]);
     }
+    printf("\n");
     r = n-1;
     l = 0;
     m = binary_search(a, n , l , r ,  k );
     if(m==0)
     {
-        printf("Element is not found .");
+        printf("\n");
+        printf("\nElement is not found . \n");
 
     }
     else 
     {
         printf("Element is found.");
     }
+    printf("\nProgram is done");
     return 0;
 
 
