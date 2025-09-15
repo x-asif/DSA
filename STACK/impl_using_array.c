0b16bfffacd79753c370cc3acd9055d82e84c889
Mohammad Asif <asifmohammad85156@gmail.com>
2025-09-15 19:56:10 +0530

    stack implementation using array try to understand the concept

---
+++ STACK/impl_using_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define X = 10;
+int string[X];
+int top = -1;
+if(top == -1){
+    printf("The strings is overflow!");
+}
+else 
+{
+    top ++;
+    push(2);
+    printf("\n");
+}
+int main (){
+    push();
+}
+  
